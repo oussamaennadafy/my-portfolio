68f2dd24e50b2074e1865af82e33ff6d145f26f8
oussama ennadafy <oussama.ennadafy@gmail.com>
2023-07-30 12:01:14 +0100

    set the page structure

---
+++ src/components/Home/Header.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+function Header() {
+  return (
+    <header className="px-12 py-4 flex justify-between items-center font-bold shadow-md text-gray-700">
+      <Link href="/">Osama.dev</Link>
+      <nav>
+        <ul className="flex">
+          <li className="py-3 px-3 hover:text-[#147efb] transition-all">
+            <Link href="#Home">Home</Link>
+          </li>
+          <li className="py-3 px-3 hover:text-[#147efb] transition-all">
+            <Link href="#About">About</Link>
+          </li>
+          <li className="py-3 px-3 hover:text-[#147efb] transition-all">
+            <Link href="#Projects">Projects</Link>
+          </li>
+          <li className="py-3 px-3 hover:text-[#147efb] transition-all">
+            <Link href="#Contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Home/Hero.js
@@ -0,0 +1,5 @@
+function Hero() {
+  return <section>div</section>;
+}
+
+export default Hero;
