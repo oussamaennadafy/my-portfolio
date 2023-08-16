d77f0389520ec09305fa6cfd8946e3425f6b1ae2
oussama ennadafy <oussama.ennadafy@gmail.com>
2023-08-16 21:43:31 +0100

    add decoration to the portfolio

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <section id="About" className="lg:px-14 px-6 lg:pt-32 pt-12">
       <div className="max-w-[1200px] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-14 lg:justify-items-start justify-items-center">
-        <div>
+        <div className="relative">
           <Image
             className="rounded-2xl"
             src="/imgs/about-img.webp"
@@ -12,6 +12,22 @@ function About() {
             height={450}
             alt="about me image"
           />
+          <div className="w-40 h-40 rounded-full bg-white absolute bottom-0 right-0 translate-y-1/4 translate-x-1/4">
+            <Image
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full animate-turn"
+              src="/imgs/full-stack-web-developer-img.png"
+              width={200}
+              height={200}
+              alt="about me image"
+            />
+            <Image
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-4/12 h-4/12"
+              src="/imgs/working-emoji.png"
+              width={100}
+              height={100}
+              alt="about me image"
+            />
+          </div>
         </div>
         <div className="max-w-xl lg:w-full text-center lg:text-left">
           <p className="uppercase font-black text-blue-500 mb-4">About me</p>
@@ -20,14 +36,13 @@ function About() {
             <span>Safi, Morocco. 📍</span>
           </h2>
           <p className="text-gray-500">
-            As a Junior Front-End Developer, I possess an impressive arsenal of
-            skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS. I excel
-            in designing and maintaining responsive websites that offer a smooth
-            user experience. My expertise lies in crafting dynamic, engaging
-            interfaces through writing clean and optimized code and utilizing
-            cutting-edge development tools and techniques. I am also a team
-            player who thrives in collaborating with cross-functional teams to
-            produce outstanding web applications.
+            "I'm a passionate Full Stack Developer skilled in HTML, CSS,
+            JavaScript, React, Node.js, Express, MongoDB, and SQL. I excel at
+            creating seamless web solutions that balance functionality and
+            aesthetics. From interactive front-ends to robust back-end systems,
+            I'm committed to efficient and up-to-date coding practices.
+            Collaborative by nature, I enjoy working across disciplines to
+            deliver exceptional web applications."
           </p>
         </div>
       </div>
